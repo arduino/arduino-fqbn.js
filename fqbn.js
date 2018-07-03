72b8c094189b39483082df5a14daa6f08fca3d26
Matteo Suppo <m.suppo@arduino.cc>
2018-07-03 15:57:27 +0200

    Use eslint and avoid exporting errors
    Fix #1, #2

--- fqbn.js
+++ fqbn.js
@@ -1,75 +1,65 @@
+'use strict';
+
 function parse(fqbn) {
-	var parts = fqbn.split(':');
+  const parts = fqbn.split(':');
 
-	// Check length
-	if (parts.length !== 3 && parts.length !== 4) {
-		throw new ErrInvalidFQBN('given fqbn has ' + parts.length + ', should have 3 or 4');
-	}
+  // Check length
+  if (parts.length !== 3 && parts.length !== 4) {
+    throw new Error('InvalidFQBN: given fqbn has ' + parts.length + ', should have 3 or 4');
+  }
 
-	// Parse config
-	var config = {};
-	if (parts.length === 4) {
-		confParts = parts[3].split(',')
-		for (var confPart of confParts) {
-			keyVal = confPart.split('=');
-			if (keyVal.length !== 2) {
-				throw new ErrInvalidFQBN('given fqbn has an invalid config: ' + confPart);
-			}
+  // Parse config
+  const config = {};
+  if (parts.length === 4) {
+    const confParts = parts[3].split(',');
+    for (const confPart of confParts) {
+      const keyVal = confPart.split('=');
+      if (keyVal.length !== 2) {
+        throw new Error('InvalidFQBN: given fqbn has an invalid config: ' + confPart);
+      }
 
-			config[keyVal[0]] = keyVal[1];
-		}
-	}
+      config[keyVal[0]] = keyVal[1];
+    }
+  }
 
-	return {
-		packager: parts[0],
-		architecture: parts[1],
-		id: parts[2],
-		config: config
-	}
+  return {
+    packager: parts[0],
+    architecture: parts[1],
+    id: parts[2],
+    config
+  };
 }
 
 function stringify(obj) {
-	var output = obj.packager + ':' + obj.architecture + ':' + obj.id;
+  let output = obj.packager + ':' + obj.architecture + ':' + obj.id;
 
-	if (!obj.config) {
-		return output;
-	}
+  if (!obj.config) {
+    return output;
+  }
 
-	var configLen = Object.keys(obj.config).length;
+  const configLen = Object.keys(obj.config).length;
 
-	if (obj.config && configLen > 0) {
-		output += ':'
-	}
+  if (obj.config && configLen > 0) {
+    output += ':';
+  }
 
-	var i = 0;
-	for (var key in obj.config) {
-		output += key + '=' + obj.config[key];
+  let i = 0;
+  for (const key in obj.config) {
+    if (Object.prototype.hasOwnProperty.call(obj.config, key)) {
+      output += key + '=' + obj.config[key];
 
-		console.log(key, i, configLen)
+      if (i < configLen - 1) {
+        output += ',';
+      }
 
-		if (i < configLen-1) {
-			output += ',';
-		}
+      i++;
+    }
+  }
 
-		i++;
-	}
-
-	return output;
-}
-
-class ErrInvalidFQBN {
-	constructor(message) {
-		this.message = message;
-	}
-
-	toString() {
-		return 'InvalidFQBN' + ': ' + this.message + '';
-	}
-
+  return output;
 }
 
 module.exports = {
-	parse: parse,
-	stringify: stringify,
-	ErrInvalidFQBN: ErrInvalidFQBN
+  parse,
+  stringify
 };
